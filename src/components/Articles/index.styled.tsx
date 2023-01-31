7338d3b01ba41001911555b466417382d8ddbcfd
Shohei Kikuchi <killchance2256@gmail.com>
2023-01-31 19:02:32 +0900

    feat: Add Article compnent

---
+++ src/components/Articles/index.styled.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledArticles = styled.div`
+  width: 100%;
+  display: block;
+  text-align: center;
+  .title {
+    margin: 10px auto;
+  }
+  .summary {
+    margin: 10px auto;
+    font-size: 18px;
+  }
+`;
+
+export default StyledArticles;
---
+++ src/components/Articles/index.tsx
@@ -0,0 +1,13 @@
+import  StyledArticles  from "./index.styled";
+
+
+const Articles = () => {
+  return (
+    <StyledArticles>
+      <h1 className="title">title</h1>
+      <p className="summary">summary</p>
+    </StyledArticles>
+  )
+}
+
+export default Articles;
