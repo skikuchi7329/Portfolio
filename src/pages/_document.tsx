d4da7805a905483dccbc4ed77100b28c21df3aa6
Shohei Kikuchi <killchance2256@gmail.com>
2023-03-07 00:02:04 +0900

    chore

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,24 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html data-theme="light">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+type Props = {}
+
+class Document extends NextDocument<Props> {
+  render() {
+    return (
+      <Html data-theme="light">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );    
+  }
 }
+
+export default Document;
