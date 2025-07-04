de2b01385670087eb4eaf0cfd5b8d0457d7f53c7
Shohei Kikuchi <killchance2256@gmail.com>
2025-07-04 19:49:49 +0900

    feat: add pre-commit hook with lint-staged and update lint-staged configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from '../components/layouts/Header';
 import Footer from '../components/layouts/Footer';
 
--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import styles from './Career.module.scss';
 
 type Props = {
