7ddb5e58d1ff2f3572eb5e81725d07d70a332622
Shohei Kikuchi <killchance2256@gmail.com>
2025-08-05 19:34:56 +0900

    fix: bio

--- src/components/Biography.tsx
+++ src/components/Biography.tsx
@@ -9,13 +9,13 @@ const Biography = () => {
         </h2>
         <div className="space-y-6">
           <p className="text-lg leading-relaxed text-justify">
-            紆余曲折を経て、理科大の数学科卒のエンジニア。
+            理科大数学科卒のエンジニア。在学時代の専攻は離散数学。
           </p>
           <p className="text-lg leading-relaxed text-justify">
-            友人のフロントエンドエンジニアを師として、独学で勉強を進め、ホームページ作成の実務を経験。
+            在学中に友人のフロントエンドエンジニアを師として、独学で勉強を進め、ホームページ作成の実務を経験。
           </p>
           <p className="text-lg leading-relaxed text-justify">
-            その後、インターン生としてフロントエンドエンジニアとしてWEBサービスの開発に従事。
+            その後、インターンのフロントエンドエンジニアとしてWEBサービスの開発に従事。
           </p>
           <p className="text-lg leading-relaxed text-justify">
             TypeScript, React, Nextjs等のモダンな技術を中心に日々勉強中。
